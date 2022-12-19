e0dc3b640e1ddac89f641bf8e7a294c312a7472c
Guilherme Santos <119808297+Gui113893@users.noreply.github.com>
2022-12-19 19:58:39 +0000

    Alterações nos atletas e index

--- wwwroot/Projeto/myScript.js
+++ wwwroot/Projeto/myScript.js
@@ -2,4 +2,17 @@
     const carousel = new bootstrap.Carousel('#myCarousel', {
         interval: 10000
     });
+
+    // Select the button and main content elements
+    const button = $('#scroll-button');
+    const mainContent = $('#main-content');
+
+    // Add a click event listener to the button
+    button.click(function() {
+      // Animate the scroll to the main content using jQuery's animate() function
+      $('html, body').animate({
+        scrollTop: mainContent.offset().top
+      }, 1); // 1 is the duration of the animation in milliseconds
+    });
+
 })
