b6750776f8fc088683001fe0c1ad81ae7822b026
Guilherme Santos <119808297+Gui113893@users.noreply.github.com>
2022-12-28 15:50:15 +0000

    Coisas no index e responsividade

--- wwwroot/Projeto/index.js
+++ wwwroot/Projeto/index.js
@@ -1,4 +1,4 @@
-﻿$('document').ready(function () {
+﻿$().ready(function () {
     const carousel = new bootstrap.Carousel('#myCarousel', {
         interval: 10000
     });
@@ -6,14 +6,33 @@
     // Select the button and main content elements
     const button = $('#scroll-button');
     const mainContent = $('#main-content');
-
+      
+    function hideScrollbar() {
+      // hide the scrollbar
+      document.body.style.overflowY = 'hidden';
+    }
+    
+    window.addEventListener('load', function() {
+      // get the current scroll position
+      var scrollTop = window.pageYOffset || document.documentElement.scrollTop;  
+      // if the scroll position is at the top of the page, hide the scrollbar
+      if (scrollTop === 0) {
+        hideScrollbar();
+      }
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+    
     // Add a click event listener to the button
     button.click(function() {
       // Animate the scroll to the main content using jQuery's animate() function
       $('html, body').animate({
         scrollTop: mainContent.offset().top
-      }, 500); // 1 is the duration of the animation in milliseconds
+      }, 500); // 500 is the duration of the animation in milliseconds
+    
+      // show the scrollbar
+      document.body.style.overflowY = 'scroll';
     });
+
 
     $('.card').on('mouseenter', function() {
       $(this).addClass('shadow-lg').css('transform', 'translateY(-5px)');
